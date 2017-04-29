bbb453e790177cc22ad262219509121807c1da78
Ricardo Canales <canalesb93@gmail.com>
2017-04-29 15:50:23 -0500

    Renamed of directories, adds interface html, js, and style

---
+++ interface/javascript/main.js
@@ -0,0 +1,34 @@
+$( document ).ready( function() {
+  var blocklyArea = document.getElementById('blocklyArea');
+  var blocklyDiv = document.getElementById('blocklyDiv');
+  var workspace = Blockly.inject(blocklyDiv, {
+    toolbox: document.getElementById('toolbox'),
+    grid: {
+      spacing: 20,
+      length: 3,
+      colour: '#ccc',
+      snap: true
+    },
+    trashcan: true
+  });
+  var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    /* var element = blocklyArea;
+      var x = 0;
+      var y = 0;
+      do {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+      } while (element);
+      // Position blocklyDiv over blocklyArea.
+      blocklyDiv.style.left = x + 'px';
+      blocklyDiv.style.top = y + 'px';
+    */
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+  };
+  window.addEventListener('resize', onresize, false);
+  onresize();
+  Blockly.svgResize(workspace);
+});
